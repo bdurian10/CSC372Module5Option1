789ec3c2b08eb164f327a82f59ebc009664848f1
Braden Durian <braden.durian@csuglobal.edu>
2024-03-17 15:18:01 -0600

    Third commit. Reworked loop for input validation in main method. Fixed multiply method not adding final number to usedNums ArrayList, resulting in only 4 numbers being displayed in the final print output

--- csc372Module5Option1/Product.java
+++ csc372Module5Option1/Product.java
@@ -12,9 +12,18 @@ public class Product {
 	public static double multiply(ArrayList<Double> unusedNumbers, ArrayList<Double> usedNumbers) {
 		//base case if 1 element left in unused nums
 		if(unusedNumbers.size() == 1) {
-			System.out.println("base case starting " + product);
+			/*
+			 * Debugging statement
+			 *System.out.println("base case starting " + product);
+			 */
 			product = product * unusedNumbers.get(0);
-			System.out.println("base case executed " + product);
+			usedNumbers.add(unusedNumbers.get(0));
+			unusedNumbers.remove(0);
+			
+			/*
+			 * Debugging Statement
+			 * System.out.println("base case executed " + product);
+			 */
 		}
 		
 		else {
@@ -25,8 +34,11 @@ public class Product {
 			unusedNumbers.remove(0);
 			usedNumbers.add(unusedNumbers.get(0));
 			unusedNumbers.remove(0);
-			System.out.println("Recursive case executed " + product);
-			
+			/*
+			 *Debugging statement 
+			 *System.out.println("Recursive case executed " + product);
+			 */
+			//Recursive call for next iteration
 			multiply(unusedNumbers, usedNumbers);
 		}
 	
@@ -34,20 +46,27 @@ public class Product {
 	}
 	
 	public static void main(String[] args) {
+		//Create scanner object for user input
 		Scanner scnr = new Scanner(System.in);
 		System.out.println("Enter 5 Numbers");
+		//Loop through asking user for input until 5 numbers collected
 		while(unusedNums.size() < 5) {
 			try {
-				System.out.println("Enter " + (5 - unusedNums.size()) + " numbers");
 				unusedNums.add(scnr.nextDouble());
+				System.out.println("Program has " + unusedNums.size() + " out of 5 numbers.");
 			}
+			//Inform user of bad inputs
 			catch(Exception e) {
 				System.out.println("Invalid Input: " + scnr.next());
 			}
 		}
+		//Close scanner when user input is complete
+		scnr.close(); 
 		
+		//Call method to multiply inputed numbers
 		double finalProduct = multiply(unusedNums, usedNums);
 		
+		//Print the result for the user
 		System.out.print("The product of ");
 		for(double num : usedNums) {
 			System.out.print(num + " ");
